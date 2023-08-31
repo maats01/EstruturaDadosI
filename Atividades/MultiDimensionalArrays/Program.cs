37dffdeed343a851f92a2da135b373fc96bdc3bf
Matheus Pelozatto Sampaio <411658@unoesc.lan>
2023-08-30 22:15:06 -0300

    aula 3

---
+++ Atividades/MultiDimensionalArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿int[,] biNumbers = new int[5,2];
+
+int[,,] triNumbers = new int[5,4,3];
+
+int[,] loadedBiNumbers = new int[,]
+{
+    {  9,   5, 15}, 
+    { 10, -11, 15},
+    {101,  45, 8}
+};
+
+for(int i = 0; i < loadedBiNumbers.GetLength(0); i++)
+{
+    for(int j = 0; j < loadedBiNumbers.GetLength(1); j++)
+    {
+        Console.Write(
+            $"[{loadedBiNumbers[i, j]}]"
+        );
+    }
+    Console.WriteLine("");
+}
---
+++ Exercícios/Exe02/Program.cs
@@ -0,0 +1,4 @@
+﻿/*atividade: 
+crie um programa que solicite ao usuário para carregar valores numa matriz 3x3 com 0 ou 1 apenas;
+o usuário também deve dizer o indice do valor a ser armazenado
+*/
