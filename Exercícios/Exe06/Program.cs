87358c10ed4f0bb9d7b6f81bfabc79d7874922cd
Matheus <matheusp.sampaio@gmail.com>
2023-09-30 12:46:54 -0300

    exe06

--- Exercícios/Exe06/Program.cs
+++ Exercícios/Exe06/Program.cs
@@ -1,1 +1,59 @@
-﻿// Livro p. 150, 151 e 152.
+﻿using Exe06;
+
+Tree<Person> tree = new Tree<Person>();
+tree.Root = new TreeNode<Person>() {
+    Data = new Person(1, "Marcin Jamro", "CEO")
+};
+
+tree.Root.Children = new List<TreeNode<Person>>
+{
+    new TreeNode<Person>{
+        Data = new Person(2, "John Smith", "Head of Development"),
+        Parent = tree.Root
+    },
+
+    new TreeNode<Person>{
+        Data = new Person(3, "Mary Fox", "Head of Research"),
+        Parent = tree.Root
+    },
+    new TreeNode<Person>{
+        Data = new Person(4, "Lily Smith", "Head of Sales"),
+        Parent = tree.Root
+    }
+};
+
+tree.Root.Children[0].Children = new List<TreeNode<Person>>
+{
+    new TreeNode<Person>{
+        Data = new Person(5, "Chris Moris", "Senior Developer"),
+        Parent = tree.Root.Children[0]
+    }
+};
+
+tree.Root.Children[1].Children = new List<TreeNode<Person>>
+{
+    new TreeNode<Person>{
+        Data = new Person(6, "Jimmy Stewart", "Senior Researcher"),
+        Parent = tree.Root.Children[1]
+    },
+    new TreeNode<Person>{
+        Data = new Person(7, "Andy Wood", "Senior Researcher"),
+        Parent = tree.Root.Children[1]
+    }
+};
+
+tree.Root.Children[2].Children = new List<TreeNode<Person>>
+{
+    new TreeNode<Person>{
+        Data = new Person(8, "Anthony Black", "Senior Sales Specialist"),
+        Parent = tree.Root.Children[2]
+    },
+    new TreeNode<Person>{
+        Data = new Person(9, "Angela Evans", "Senior Sales Specialist"),
+        Parent = tree.Root.Children[2]
+    },
+    new TreeNode<Person>{
+        Data = new Person(10, "Tommy Butler", "Senior Sales Manager"),
+        Parent = tree.Root.Children[2]
+    }
+};
